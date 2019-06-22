a1a5378398f7775c292a02030cef7653aca82c9b
cellphonedude <cellphonedude@users.noreply.github.com>
2019-06-22 14:00:41 -0700

    1stboot.sh
    Script to setup pi zero/3b (not b+) with nexmon drivers

---
+++ scripts/1stboot.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt update && sudo apt upgrade -y
+sudo apt autoremove -y
+sudo rm -v /etc/ssh/ssh_host_*
+sudo dpkg-reconfigure openssh-server
+sudo systemctl restart ssh
+sleep 10
+sudo reboot
+exit 0
