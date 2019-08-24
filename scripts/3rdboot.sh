06b33be108d0e13dca7c523abd7000658fff0bbf
cellphonedude <cellphonedude@users.noreply.github.com>
2019-08-24 16:33:15 -0700

    Update 3rdboot.sh

--- scripts/3rdboot.sh
+++ scripts/3rdboot.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt install autossh haveged screen ppp bash-completion festival gpsd spectools aircrack-ng dnsmasq hostapd build-essential git pkg-config wireshark tcpdump nmap raspberrypi-kernel-headers gawk qpdf bison flex make bc libncurses5-dev libgmp3-dev -y
+sudo apt install raspberrypi-kernel-headers git libgmp3-dev gawk qpdf bison flex make bc -y
 sudo wget https://raw.githubusercontent.com/notro/rpi-source/master/rpi-source -O /usr/bin/rpi-source && sudo chmod +x /usr/bin/rpi-source && /usr/bin/rpi-source -q --tag-update
 sudo rpi-source
 sleep 10
